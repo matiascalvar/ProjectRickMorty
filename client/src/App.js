59342a222a44ff7dc407f113f27bebc74d2d4e60
matiascalvar <matiasncalvar@gmail.com>
2021-11-19 19:03:46 -0300

    working on the nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import Nav from './components/Nav'
 
 
 function App() {
   return (
     <div className='App'>
+    <Nav/>
       <Routes>
         <Route path="/" element={ <h1>Crear componente para mostrar los primeros 20 chars de la serie</h1> }/>
+        
         <Route path="/:name" element={ <h1>Crear componente para mostrar por nombre</h1> }/>
-       
       </Routes>
     </div>
   )
---
+++ client/src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Nav () {
+    return (
+        <nav>
+            <form className="">
+                <input className="" placeholder="Ingrese un nombre..."/>
+                <button className="btn btn-primary" type="submit">Buscar</button>
+            </form>
+        </nav>
+    )
+}
